114fc3c2756d4b7b05767ba397aab05fccbbb796
Delwaulle <delwaulle.loic@hotmail.fr>
2014-04-15 14:52:53 +0200

    Mouahahha

--- virtualWar1.0/src/gameController/EntrerAction.java
+++ virtualWar1.0/src/gameController/EntrerAction.java
@@ -3,11 +3,11 @@ package gameController;
 import java.util.Scanner;
 
 public class EntrerAction {
-
+	// Trouve moi robin !!!
 	public static String entrerAction () {
 		Scanner sc = new Scanner(System.in);
 		String rep="";
-		while (!rep.equals("attaquer") && !rep.equals("deplacer") && !rep.equals("stat")) 
+		while (!rep.equals("attaquer") && !rep.equals("deplacer") && !rep.equals("stat"))
 			rep = sc.nextLine();
 		return rep;
 	}
