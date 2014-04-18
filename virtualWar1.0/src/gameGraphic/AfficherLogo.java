5312104fd91df9d2e7b47c0a567999c26fac0d78
dauchy <dauchyrobin@gmail.com>
2014-04-18 13:03:52 +0200

    d

---
+++ virtualWar1.0/src/gameGraphic/AfficherLogo.java
@@ -0,0 +1,119 @@
+package gameGraphic;
+
+import java.util.Scanner;
+
+public class AfficherLogo {
+
+	public static void afficherLogo() {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner (System.in);
+		System.out.println("\n\n\n\n      _______________ ");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    /_______________/|");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |   |_  \\     | ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |     \\  \\    | ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |      \\  \\   | ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |       \\  \\  | ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |        \\  \\ | ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |         \\  \\| ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |          \\    ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |           \\   ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |_______________||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |  B R O N Z E  ||");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("    |_______________|/\n\n\n");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("*****************************");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("*                           *");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("*        VIRTUAL WAR        *");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("*                           *");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("*****************************");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("Bienvenue dans la 1ere version de Virtual War");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("Pour les directions, veuillez entrer la direction en toute lettre (haut, bas, gauche ou droite)");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("Pour les diagonales, veuillez entrer la direction verticale, puis horizontale)");
+		try{
+			Thread.sleep(100);
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+		System.out.println("Tapez ENTRER pour lancer la partie! ");
+		sc.nextLine();
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+}
