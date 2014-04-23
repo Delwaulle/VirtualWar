2e442289e9827bde8123b8cbc5c4588fd7bcfd2f
Delwaulle <delwaulle.loic@hotmail.fr>
2014-04-23 13:17:23 +0200

    Ajout du bouclage du son à l'infini ainsi qu'un JCheckBox afin de désactiver le son dans l'écran d'acceuil.

--- virtualWar1.0/src/gameGraphic/MenuDemarrage.java
+++ virtualWar1.0/src/gameGraphic/MenuDemarrage.java
@@ -19,17 +19,27 @@ public class MenuDemarrage extends JPanel{
 	JButton bouton3;
 	JPanel p;
 	JPanel p2;
+	JPanel p3;
 	SonMenu son;
+	CheckBoxSon cbs;
 
 	public MenuDemarrage()  {
+		son=new SonMenu();
+		son.setDaemon(true);
+		son.start();
 		bouton1 =  new Bouton("Lancez une partie");
 		bouton2 =  new Bouton("Sauvegardes");
 		bouton3 = new Bouton("Paramètres");
+		cbs=new CheckBoxSon("Désactiver le son",son);
+		p3=new JPanel();
+		p3.setBackground(Color.BLACK);
+		p3.setLayout(new FlowLayout(0,335,0));
+		p3.add(cbs);
 		p=new JPanel();
 		p2=new JPanel();
 		p2.setBackground(Color.BLACK);
 		p.setBackground(Color.BLACK);
-		p2.setLayout(new FlowLayout(200,260,150));
+		p2.setLayout(new FlowLayout(150,252,150));
 		this.setLayout(new BorderLayout());
 		p.setLayout(new GridLayout(3,1,0,60));
 		p.add(bouton1);
@@ -37,8 +47,6 @@ public class MenuDemarrage extends JPanel{
 		p.add(bouton3);
 		p2.add(p);
 		this.add(BorderLayout.CENTER,p2);
-		son=new SonMenu();
-		son.setDaemon(true);
-		son.start();
+		this.add(BorderLayout.SOUTH,p3);
 	}
 }
--- virtualWar1.0/src/gameGraphic/SonMenu.java
+++ virtualWar1.0/src/gameGraphic/SonMenu.java
@@ -15,54 +15,58 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class SonMenu extends Thread{
 	AudioInputStream audioInputStream = null;
 	SourceDataLine line;
+	private boolean dodo;
 	@Override
 	public void run(){
 		File fichier = new File("son/SP0000.WAV");
-		try {
-			@SuppressWarnings("unused")
-			AudioFileFormat format = AudioSystem.getAudioFileFormat(fichier);
-		} catch (UnsupportedAudioFileException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(fichier);
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		AudioFormat audioFormat = audioInputStream.getFormat();
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class,audioFormat);
-
-		try {
-			line = (SourceDataLine) AudioSystem.getLine(info);
-
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		}
-
-		try {
-			line.open(audioFormat);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		}
-		line.start();
-
-		try {
-			byte bytes[] = new byte[1024];
-			int bytesRead=0;
-			while ((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1) {
-				line.write(bytes, 0, bytesRead);
+		while (!dodo) {
+			try {
+				@SuppressWarnings("unused")
+				AudioFileFormat format = AudioSystem.getAudioFileFormat(fichier);
+			} catch (UnsupportedAudioFileException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
-		} catch (IOException io) {
-			io.printStackTrace();
-			return;
+
+			try {
+				audioInputStream = AudioSystem.getAudioInputStream(fichier);
+			} catch (UnsupportedAudioFileException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			AudioFormat audioFormat = audioInputStream.getFormat();
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class,audioFormat);
+
+			try {
+				line = (SourceDataLine) AudioSystem.getLine(info);
+
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+				return;
+			}
+
+			try {
+				line.open(audioFormat);
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+				return;
+			}
+			line.start();
+
+			try {
+				byte bytes[] = new byte[1024];
+				int bytesRead=0;
+				while ((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1) {
+					line.write(bytes, 0, bytesRead);
+				}
+			} catch (IOException io) {
+				io.printStackTrace();
+				return;
+			}
 		}
+
 	}
 }
