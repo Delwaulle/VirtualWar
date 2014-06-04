db9dc28b7d26cb625d1139ae608d5fcff181bdf9
Delwaulle <delwaulle.loic@hotmail.fr>
2014-06-04 20:21:55 +0200

    Ajout area carac

--- virtualWar1.0/src/gameGraphic/WarPanel.java
+++ virtualWar1.0/src/gameGraphic/WarPanel.java
@@ -18,6 +18,7 @@ public class WarPanel extends JPanel{
 	public static JPanel p;
 	public static BoardDisplayer bd;
 	public static JTextArea t;
+	public static JTextArea t2;
 	public static String info="coucou";
 
 	public WarPanel(CardLayout cl, JPanel pan){
@@ -33,10 +34,13 @@ public class WarPanel extends JPanel{
 
 		p.add(new ActionPanel(1));
 		t=new JTextArea(info);
+		t2=new JTextArea("carac");
 
 
 		t.setBounds(5,20,350,60);
+		t2.setBounds(5,130,350,60);
 		p2.add(t);
+		p2.add(t2);
 		p.add(p2);
 		tours++;
 		this.add(bd);
